d083461bb8c8299a3ffade7ae44012d9490c75ae
Taimoor13003 <taimoorghori@rocketmail.com>
2022-11-11 18:52:39 +0700

    uncommenting cloudinary configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,33 @@
 // // Update here your cloudinary access credential here
 
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//         cloud_name: env("CLOUDINARY_NAME"),
-//         api_key: env("CLOUDINARY_KEY"),
-//         api_secret: env("CLOUDINARY_SECRET"),
-//       },
-//       actionOptions: {
-//         upload: {},
-//         uploadStream: {},
-//         delete: {},
-//       },
-//     },
-//   },
-//   gatsby: {
-//     enabled: true,
-//     headers: {
-//       "x-gatsby-cloud-data-source": "@relate-app/gatsby-source-strapi",
-//     },
-//   },
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  gatsby: {
+    enabled: true,
+    headers: {
+      "x-gatsby-cloud-data-source": "@relate-app/gatsby-source-strapi",
+    },
+  },
 //   // 'gatsby-preview': {
 //   //   enabled : true,
 //   //   // resolve : './src/plugins/gatsby-preview'
 //   //   resolve : '../src/plugins/gatsby-preview'
 //   // },
-// });
+});
 
 // // module.exports = ({ env }) => ({
 // //     upload: {
